1148f571f6a4b849c070f13fc5f44fc0c79f4767
Dunay <adream0@gmail.com>
2022-05-09 16:26:27 +0100

    DI methods

---
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author jasim
+ */
+class PropertyInjectedControllerTest {
+PropertyInjectedController controller;
+
+@BeforeEach
+void setup()
+    {
+    controller= new PropertyInjectedController();
+
+    controller.greetingService= new GreetingServiceImpl();
+
+    }
+
+
+    @Test
+    void getGreeting() {
+    System.out.println(controller.getGreeting());
+
+    }
+}
