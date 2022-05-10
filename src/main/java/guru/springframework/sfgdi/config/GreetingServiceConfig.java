e81811538f5c46b331b578d587c2af4bdb9f8f30
Dunay <adream0@gmail.com>
2022-05-10 16:42:41 +0100

    Configuration class sample

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author jasim
+ * Beans of external components. Also Config file to define beans instead of class notations
+ */
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService()
+    {
+        return new ConstructorGreetingService();
+    }
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService()
+    {
+        return new PropertyInjectedGreetingService();
+    }
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService()
+    {
+        return new SetterInjectedGreetingService();
+    }
+
+
+}
